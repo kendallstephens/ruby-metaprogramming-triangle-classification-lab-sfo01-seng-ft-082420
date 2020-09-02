513c33351aec53cbea8f1b63f98584b70faf3190
kendallstephens <kendoll.stephens@gmail.com>
2020-09-02 15:57:25 -0700

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
 class Triangle
-  # write code here
+  attr_reader :a, :b, :c
+  
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c 
 end
+
+
+def valid_triangle
+  true_triangle = [(a + b > c), (b + c > a), (a + c > b)]
+  [a, b, c].each do |side|
+    true_triangle << false if side <= 0
+    raise TriangleError if true_triangle.include?(false)
+  end
+end 
+
+def kind
+  valid_triangle
+  if a == b && b == c 
+    :equilateral
+  elsif a == b || b == c || c == a 
+    :isosceles
+  else
+    :scalene
+  end
+end
+
+  class TriangleError < StandardError
+    end
+  end
+
